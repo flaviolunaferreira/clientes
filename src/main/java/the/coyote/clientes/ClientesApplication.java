4a0b2900d7b4973a966c7be434720f3d87fea419
theCoyote <flaviolunaferreira@gmail.com>
2024-08-30 14:41:18 -0300

    tirando discorery do cliente

--- src/main/java/the/coyote/clientes/ClientesApplication.java
+++ src/main/java/the/coyote/clientes/ClientesApplication.java
@@ -2,13 +2,11 @@ package the.coyote.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ClientesApplication {
 
 	public static void main(String[] args) {
