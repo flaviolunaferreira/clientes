9e62715a76ca4a788c23deda09058f5c1c06854d
theCoyote <flaviolunaferreira@gmail.com>
2024-08-30 17:40:18 -0300

    teste5

--- src/main/java/the/coyote/clientes/ClientesApplication.java
+++ src/main/java/the/coyote/clientes/ClientesApplication.java
@@ -13,9 +13,6 @@ public class ClientesApplication {
 
 	public static void main(String[] args) {
 
-		String profile = System.getenv("PROFILE");
-		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
-		log.debug("Waiting fo Internal Interface to start...");
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
